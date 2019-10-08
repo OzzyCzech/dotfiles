c7e99ed6221b027203fb84c0d9f46226d25becef
Roman Ožana <roman@omdesign.cz>
2019-10-08 14:23:47 +0200

    Backup brew

--- brew/brew.sh
+++ brew/brew.sh
@@ -5,7 +5,7 @@
 #############################################################################
 
 if ! command -v brew >/dev/null 2>&1 ; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else    
     echo "\033[0;32m[OK]\033[0m brew already installed"
 fi;
@@ -16,10 +16,10 @@ fi;
 
 while read -r tap; do    
     if ! brew tap | grep "caskroom/cask"  &>/dev/null ; then
-        echo "\033[0;31m[MISSING]\033[0m $tap..."
+        printf "\033[0;31m[MISSING]\033[0m $tap..."
         brew tap $tap
     else 
-        echo "\033[0;32m[OK]\033[0m $tap already tapped"
+        printf "\033[0;32m[OK]\033[0m $tap already tapped"
     fi;
 done < "./tap.txt"
 
@@ -32,11 +32,11 @@ brew cask update
 #############################################################################
 
 while read -r cask; do    
-    if ! brew cask ls --versions $cask >/dev/null 2>&1; then
-        echo "\033[0;31m[MISSING]\033[0m $cask..."
+    if ! brew cask ls --versions ${cask} >/dev/null 2>&1; then
+        printf "\033[0;31m[MISSING]\033[0m $cask..."
         brew cask install $cask
     else 
-        echo "\033[0;32m[OK]\033[0m $cask already installed"
+        printf "\033[0;32m[OK]\033[0m $cask already installed"
     fi;
 done < "./cask-list.txt"
 
@@ -44,13 +44,12 @@ done < "./cask-list.txt"
 # Brew packages
 #############################################################################
 
-
-while read -r package; do    
+while read -r package; do
     if ! brew ls --versions $package >/dev/null 2>&1; then
-        echo "\033[0;31m[MISSING]\033[0m $package..."
+        printf "\033[0;31m[MISSING]\033[0m $package..."
         brew install $package
     else 
-        echo "\033[0;32m[OK]\033[0m $cask already installed"
+        printf "\033[0;32m[OK]\033[0m $cask already installed"
     fi;
 done < "./list.txt"
 
@@ -58,8 +57,8 @@ done < "./list.txt"
 # Upgrade already installed
 #############################################################################
 
-if [ -x "$(brew outdated)" ]; then
-    echo "[INFO] there are some outdated packages"
+if [[ -x "$(brew outdated)" ]]; then
+    printf "[INFO] there are some outdated packages"
     brew outdated
     read -p "Continue installing outdated brews [y]? " -n 1 -r
     echo    # (optional) move to a new line
