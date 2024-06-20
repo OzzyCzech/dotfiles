1292d6127555de1c30c9e2a34f1c7ae6f01470cd
Roman Ožana <roman@ozana.cz>
2024-06-20 12:20:04 +0200

    Password generation

--- setup/set-screensaver.zsh
+++ setup/set-screensaver.zsh
@@ -7,14 +7,14 @@ function set-screen-saver() {
 
   info "Screen saver settings"
 
-  # Start screen saver after 5 minutes
-  defaults -currentHost write com.apple.screensaver idleTime -int 300
-  ok "Start screen saver after 5 minutes"
+  # Start screen saver after 10 minutes
+  defaults -currentHost write com.apple.screensaver idleTime -int 600
+  ok "Start screen saver after 10 minutes"
 
   # Require password immediately after sleep or screen saver begins
   defaults write com.apple.screensaver askForPassword -int 1
   defaults write com.apple.screensaver askForPasswordDelay -int 0
-  ok "Require password immediately after sleep or screen saver begins"
+  ok "Require password immediately"
 
   killall "System Preferences" &> /dev/null
 }
--- zsh/password.zsh
+++ zsh/password.zsh
@@ -1,4 +1,6 @@
-function password() {
+function random-password() {
   local length="${1:-24}"
   ruby -r securerandom -e "puts SecureRandom.base64($length)" | tr -d "[:punct:]" 2> /dev/null
 }
+
+alias mkpasswd=random-password
