80313d4eb3bf13914ebd33404d9c3ba0aeeb5f42
Roman Ožana <roman@ozana.cz>
2026-06-28 07:34:18 +0200

    Drop unset Mail defaults, keep only AddLinkPreviews

--- setup/apps/mail.zsh
+++ setup/apps/mail.zsh
@@ -4,19 +4,6 @@
 # Mail
 ###############################################################################
 
-# Disable send and reply animations in Mail.app
-defaults write com.apple.mail DisableReplyAnimations -bool true
-defaults write com.apple.mail DisableSendAnimations -bool true
-
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
-# Disable inline attachments (just show the icons)
-defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
-
-# Mark all messages as read when opening a conversation
-defaults write com.apple.mail ConversationViewMarkAllAsRead -bool true
-
 # Disable link previews in messages
 defaults write com.apple.mail AddLinkPreviews -bool false
 
